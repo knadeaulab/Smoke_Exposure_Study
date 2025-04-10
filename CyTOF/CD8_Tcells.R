6704c1b008fef5ccd7e375b09082e7e621efd7ff
knadeaulab <akaushik@hsph.harvard.edu>
2025-04-09 19:53:47 -0700

    Update CD8_Tcells.R

--- CyTOF/CD8_Tcells.R
+++ CyTOF/CD8_Tcells.R
@@ -37,8 +37,8 @@ fm = c("CD27","CD28","CD154", "CD294","CD185", "CD62L", "CD183", "CD194", "CD196
         "CXCR5","CXCR3","CCR4","CCR6","CCR7","CCR5","PD.1", "CRTH2")
 functional_markers = unique(c(fm,toxicMetals))
 
-load("Files/CD8.Rdata")
-load("Files/CD8_T_HandGated_subsets.Rdata")
+load("Data/CD8.Rdata")
+load("Data/CD8_T_HandGated_subsets.Rdata")
 
 colrs = c( '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', 
            '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', 
